0d185c5bdc9bf22d74a10dc927b2c4f5cd9ee09d
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2025-02-27 16:45:06 +0700

    test

--- site/src/Controller/DisplayController.php
+++ site/src/Controller/DisplayController.php
@@ -66,9 +66,8 @@ class DisplayController extends \Joomla\CMS\MVC\Controller\BaseController
 		var_dump('Requested Config ID: ' . $requested_config_id);
 		var_dump('Config ID: ' . $config->id);
 		if (!empty($config) && $requested_config_id > 0 && $config->id != $requested_config_id) {
+			$requested_config_id = (int) $this->app->setUserState('com_dt_whatsapp_tenants_configs.edit.whatsapptenantsconfig.id', $config->id);
 			throw new \Exception('Unauthorized access');
-			// $this->app->setUserState('com_dt_whatsapp_tenants_configs.edit.whatsapptenantsconfig.id', $config->id);
-			// var_dump('Config set');		
 		}
 		
 		// $requested_config_id = (int) $this->app->getUserState('com_dt_whatsapp_tenants_configs.edit.whatsapptenantsconfig.id');
